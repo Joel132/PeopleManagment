7cea95f8ca8153c402a252fb4e91e31720cac2f1
juanca <juancanhiza@gmail.com>
2019-01-09 14:13:36 -0300

    Agregué el Material al proyecto de Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+// Animations
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+// Angular Material
+import {MaterialModule} from './angular_material';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
