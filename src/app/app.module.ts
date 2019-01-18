a4faf300c5741331b0a7fab1a6bc13011c36fffa
Carlos Cabrera <cadacaes@gmail.com>
2019-01-18 10:24:52 -0300

    Creación de componente Login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,12 @@ import { AppComponent } from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 // Angular Material
 import {MaterialModule} from './angular_material';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
